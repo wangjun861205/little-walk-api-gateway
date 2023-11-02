91038aa1c099730b27d66f62c32e072e0deb8695
wangjun <444055828@qq.com>
2023-11-02 21:43:36 +0800

    checkpoint

--- src/core/dog_client.rs
+++ src/core/dog_client.rs
@@ -2,6 +2,6 @@ use anyhow::Error;
 use bytes::Bytes;
 
 pub trait DogClient {
-    async fn add_dog(&self, owner_id: &str, body: Bytes) -> Result<String, Error>;
+    async fn add_dog(&self, owner_id: &str, body: Bytes) -> Result<Bytes, Error>;
     async fn dogs_by_owner_id(&self, owner_id: &str) -> Result<Bytes, Error>;
 }
